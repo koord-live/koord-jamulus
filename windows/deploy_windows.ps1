7450ddcb268ed870316b7733e57221c6b315a59f
Dan G <dan.garton@gmail.com>
2021-07-27 18:01:37 +0200

    hardcode cmake path

--- windows/deploy_windows.ps1
+++ windows/deploy_windows.ps1
@@ -171,7 +171,7 @@ Function Initialize-Build-Environment
 
     # Setup Qt executables paths for later calls
     Set-Item Env:QtQmakePath "$QtMsvcSpecPath\qmake.exe"
-    Set-Item Env:QtCmakePath  "$QtPath\Tools\CMake_64\bin\cmake.exe"
+    Set-Item Env:QtCmakePath  "C:\Qt\Tools\CMake_64\bin\cmake.exe" #FIXME should use QtPath
     Set-Item Env:QtWinDeployPath "$QtMsvcSpecPath\windeployqt.exe"
 
     ""
