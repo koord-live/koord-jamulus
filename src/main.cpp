7ffa7b57f9eb1b97473383c61e44658f59fa2733
Dan G <dan.garton@gmail.com>
2022-06-11 19:25:50 +0200

    set style and dark stylesheet for application

--- src/main.cpp
+++ src/main.cpp
@@ -935,7 +935,28 @@ int main ( int argc, char** argv )
     // need this before new QApplication
     QtWebView::initialize();
 
-    QCoreApplication* pApp = bUseGUI ? new QApplication ( argc, argv ) : new QCoreApplication ( argc, argv );
+    //FIXME - gui vs nogui handling
+//    QCoreApplication* pApp = bUseGUI ? new QApplication ( argc, argv ) : new QCoreApplication ( argc, argv );
+    QApplication* pApp = bUseGUI ? new QApplication ( argc, argv ) : new QApplication ( argc, argv );
+    pApp->setStyle("fusion");
+
+    // Now use a palette to switch to dark colors:
+    QPalette palette = QPalette();
+    palette.setColor(QPalette::Window, QColor(53, 53, 53));
+    palette.setColor(QPalette::WindowText, Qt::white);
+    palette.setColor(QPalette::Base, QColor(25, 25, 25));
+    palette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
+    palette.setColor(QPalette::ToolTipBase, Qt::black);
+    palette.setColor(QPalette::ToolTipText, Qt::white);
+    palette.setColor(QPalette::Text, Qt::white);
+    palette.setColor(QPalette::Button, QColor(53, 53, 53));
+    palette.setColor(QPalette::ButtonText, Qt::white);
+    palette.setColor(QPalette::BrightText, Qt::red);
+    palette.setColor(QPalette::Link, QColor(42, 130, 218));
+    palette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    palette.setColor(QPalette::HighlightedText, Qt::black);
+    pApp->setPalette(palette);
+
 #    endif
 #endif
 
