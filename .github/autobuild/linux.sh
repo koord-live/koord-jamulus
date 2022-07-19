60247dfce354ad6140c24b68aab65a70363b68ef
Dan G <dan.garton@gmail.com>
2022-07-19 11:17:38 +0200

    linux build qt don't specifiy archives

--- .github/autobuild/linux.sh
+++ .github/autobuild/linux.sh
@@ -56,8 +56,8 @@ setup_x64() {
     # Note: icu archive is needed on Linux unlike others
     # - and webchannel and positioning modules, only needed for qmake on Linux - don't know why!
     sudo python3 -m aqt install-qt --outputdir "${QT_DIR}" linux desktop "${QT_VERSION}" \
-        --archives qtbase qtdeclarative qttools icu \
         --modules qtwebview qtwebengine qtwebchannel qtpositioning
+        # --archives qtbase qtdeclarative qttools icu \
 
 }
 
