0fc2a0023ea3aaaa699e8747c66200d304fb62f5
Dan G <dan.garton@gmail.com>
2022-08-01 19:24:15 +0200

    linux fix openssl v1 v3 issue

--- linux/deploy_x64_appimg.sh
+++ linux/deploy_x64_appimg.sh
@@ -53,7 +53,9 @@ find ../appdir_gui
 echo "Building gui AppImage ...."
 cd $BDIR
 # manually copy in qml files to get picked up by qmlimportscanner
-cp src/webview.qml appdir_gui
+cp -v src/webview.qml appdir_gui
+# manually copy in libssl v1, we need to ship to stop breakage on systems expecting v3
+cp -v /usr/lib/x86_64-linux-gnu/libssl.so.1.1  appdir_gui/lib
 linuxdeploy -d linux/koordrt.desktop -i linux/koordrt.png --appdir appdir_gui --plugin qt --output appimage
 mkdir gui_appimage
 mv Koord-RT-*.AppImage gui_appimage/Koord-RT-${VERSION}_x64.appimage
