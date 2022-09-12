8aa7fe0cef6748c4aa23921bf2be802df1a5cbc3
Dan G <dan.garton@gmail.com>
2022-09-12 16:03:51 +0200

    ios remove .xcode dir

--- ios/deploy_ios.sh
+++ ios/deploy_ios.sh
@@ -53,7 +53,8 @@ build_ipa()
     mv Koord.pro Koord-RT.pro
     qmake -spec macx-xcode Koord-RT.pro
 
-    # rm -fr .xcode
+    echo "Removing .xcode/...."
+    rm -fr .xcode
 
     # disable deprecation warnings re legacy build system - XCode 13 errors on this
     # /usr/libexec/PlistBuddy -c "Add :DisableBuildSystemDeprecationDiagnostic bool" Koord-RT.xcodeproj/project.xcworkspace/xcshareddata/WorkspaceSettings.xcsettings
