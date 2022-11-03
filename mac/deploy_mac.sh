4bb18f78369b490676381b003fd383475d38029c
Dan G <dan.garton@gmail.com>
2022-11-03 16:25:21 +0200

    fix typo

--- mac/deploy_mac.sh
+++ mac/deploy_mac.sh
@@ -83,7 +83,7 @@ cleanup() {
 build_app_compile_universal()
 {
     local posix_mode="${1}"
-    if [[ ${posix_mode} == "posixmac"]]; then
+    if [[ ${posix_mode} == "posixmac" ]]; then
         EXTRADEFINES="DEFINES+=POSIXMAC"
     else
         EXTRADEFINES=""
