842bf547bda92d2305a578ad18cb29d0b1bfad49
Dan G <dan.garton@gmail.com>
2022-11-05 14:19:46 +0200

    mac fix dmg banner img

--- mac/deploy_mac.sh
+++ mac/deploy_mac.sh
@@ -252,7 +252,7 @@ build_disk_image()
     # Build installer image
     create-dmg \
       --volname "${client_target_name} Installer" \
-      --background "${resources_path}/installerbackground.png" \
+      --background "${resources_path}/MacInstallerBanner.png" \
       --window-pos 200 400 \
       --window-size 900 320 \
       --app-drop-link 820 210 \
